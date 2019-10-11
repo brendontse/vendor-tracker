203a9937d4303ecf7556cfb4aa0b869aa3eec193
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-10-11 08:43:05 -0700

    create initial file structure

---
+++ vendor-tracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vendor_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
