c22f16a768f9ef346f87173d5749b9d9aca0682c
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-10-11 13:45:20 -0700

    fix Program.cs and Startup.cs, add Order and Vendor classes to MVC structure

--- vendor-tracker/Program.cs
+++ vendor-tracker/Program.cs
@@ -8,7 +8,14 @@ namespace vendor_tracker
     {
         public static void Main(string[] args)
         {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
 
+            host.Run();
         }
     }
 }
