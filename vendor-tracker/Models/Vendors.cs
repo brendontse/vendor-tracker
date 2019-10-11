66ed47b6d7cf6e75e21cd2d000475551f9390eee
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-10-11 16:05:31 -0700

    add Vendor object constructor

--- vendor-tracker/Models/Vendors.cs
+++ vendor-tracker/Models/Vendors.cs
@@ -6,13 +6,23 @@ namespace vendor_tracker.Models
     public class Vendor
     {
         public int ID { get; }
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public List<Order> VendorOrders {get; set;}
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Order> VendorOrders { get; set; }
 
         public static List<Vendor> VendorList = new List<Vendor> { };
-        public static int VendorNumber { get; set;}
+        public static int VendorNumber { get; set; }
 
+        public Vendor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            VendorOrders = new List<Order> { };
+            VendorNumber++;
+            ID = VendorNumber;
+            VendorList.Add(this);
+        }
+        
 
     }
 }
